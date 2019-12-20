f95c1bfc7c51a301e28486fba430770347e474b1
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2019-12-20 08:09:04 +0000

    Deploy oswalth/2019-2-Atom-Frontend-V-Carpa to github.com/oswalth/2019-2-Atom-Frontend-V-Carpa.git:gh-pages

---
+++ precache-manifest.1f7c9690d8ea7d2fd6261dfee61ddf3b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "324335ef56a023f57552776b1c5c9962",
+    "url": "/2019-2-Atom-Frontend-V-Carpa/index.html"
+  },
+  {
+    "revision": "9412ba252ea8259c33fa",
+    "url": "/2019-2-Atom-Frontend-V-Carpa/static/css/main.5948ed0a.chunk.css"
+  },
+  {
+    "revision": "dd4e385daf61820a3bad",
+    "url": "/2019-2-Atom-Frontend-V-Carpa/static/js/2.bb846510.chunk.js"
+  },
+  {
+    "revision": "84a2a7a390df1ac99fa4fadf23155e60",
+    "url": "/2019-2-Atom-Frontend-V-Carpa/static/js/2.bb846510.chunk.js.LICENSE"
+  },
+  {
+    "revision": "9412ba252ea8259c33fa",
+    "url": "/2019-2-Atom-Frontend-V-Carpa/static/js/main.dde3320e.chunk.js"
+  },
+  {
+    "revision": "06adcb68a84896b418aa",
+    "url": "/2019-2-Atom-Frontend-V-Carpa/static/js/runtime-main.79e45abb.js"
+  }
+]);
